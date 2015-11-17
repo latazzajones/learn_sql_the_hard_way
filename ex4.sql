76b0d5a0b5f3ba43bf8d7689ade685dac46afe3c
Natasha Jones <tasha.k.jones@gmail.com>
2015-11-17 06:19:11 -0500

    finish excercise 5

---
+++ ex4.sql
@@ -0,0 +1,6 @@
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+
+INSERT INTO person VALUES (1, "Tasha", "Jones", 31);
+INSERT INTO pet VALUES (2, "Plant", 5, "Plant", 0);
+INSERT INTO person_pet VALUES (1, 2);
---
+++ notes.md
@@ -0,0 +1,8 @@
+###Importing building db schema from sql file
+*`sqlite3 db_name.db < sql_file_name.sql`
+
+###Viewing schema
+*`sqlite3 db_name.db .schema`
+
+###Quitting out of sqlite3
+*`.quit`
